6a9fda0298ad4a5bdeb6eaa565d6dc8c6445b89d
timoko3 <freepower27@gmail.com>
2026-09-14 22:11:52 +0300

    Separate task2 debug and release builds

--- task_2/solution/echoTest.sh
+++ task_2/solution/echoTest.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-./genTestFile.sh
-./fullDuplexPipe testFile testFile.out
+bash ./genTestFile.sh
+"${1:-./build/debug/fullDuplexPipe}" testFile testFile.out
 md5sum testFile testFile.out
 rm testFile testFile.out
