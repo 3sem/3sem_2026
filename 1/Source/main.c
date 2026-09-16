22e50f0bf5d86f2c6268772e5afddb02d8f4fd31
Ovsienko2007 <i_dont_now2@mail.ru>
2026-09-16 17:30:37 +0300

    замена %m[^\n] на %[^\n]

--- 1/Source/main.c
+++ 1/Source/main.c
@@ -2,12 +2,12 @@
 #include "include.h"
 
 int main() {
-    char *command = NULL;
+    char command[1024];
 
     while (1){
         cmd_arr_t *cmd_arr = create_cmd_arr();
 
-        if (scanf(" %m[^\n]", &command) == -1){
+        if (scanf(" %[^\n]", command) == -1){
             printf("exit\n");
 
             destroy_cmd_arr(cmd_arr);
@@ -18,8 +18,6 @@ int main() {
             printf("exit\n");
 
             destroy_cmd_arr(cmd_arr);
-            free(command);
-
             break;
         }
 
@@ -28,7 +26,6 @@ int main() {
         seq_pipeline(cmd_arr->arr);
 
         destroy_cmd_arr(cmd_arr);
-        free(command);
     }
 
     return 0;
